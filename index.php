2cdc1e438b158f6a36086214de5a4abb44223585
Zhenchick <evgesha_37rus@mail.ru>
2018-05-09 16:51:44 +0300

    Add first function

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-$is_auth = (bool) rand(0, 1);
 
-$user_name = 'Константин';
-$user_avatar = 'img/user.jpg';
+require "functions.php";
+
+require "data.php";
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -56,24 +57,16 @@ $user_avatar = 'img/user.jpg';
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="promo__item promo__item--attachment">
-                <a class="promo__link" href="all-lots.html">Крепления</a>
-            </li>
-            <li class="promo__item promo__item--boots">
-                <a class="promo__link" href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="promo__item promo__item--clothing">
-                <a class="promo__link" href="all-lots.html">Одежда</a>
-            </li>
-            <li class="promo__item promo__item--tools">
-                <a class="promo__link" href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="promo__item promo__item--other">
-                <a class="promo__link" href="all-lots.html">Разное</a>
-            </li>
+             <!-- Выводит верхнее меню -->
+            <?php foreach ($categories as $i => $category):  ?>
+
+                   <li class="promo__item promo__item--<?=$category['code']; ?>">
+                    <a class="promo__link" href="all-lots.html">
+                        <?=$category['title']; ?>
+                    </a>
+                   </li>
+
+            <?php endforeach; ?>
         </ul>
     </section>
     <section class="lots">
@@ -81,24 +74,32 @@ $user_avatar = 'img/user.jpg';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
 
+            <!-- Выводит лоты аукциона -->
+            <?php foreach ($lots_list as $lot):  ?>
+
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?=$lot['img'] ?>" width="350" height="260" alt="<?=$lot['name'] ?>">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$lot['category'] ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot['name'] ?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost">
+                                    <!-- Форматирует начальную цену, добавляет знак рубля -->
+                                    <?=formatPrice($lot['price']) ?>
+                                </span>
+                            </div>
+                            <div class="lot__timer timer">
+
+                            </div>
                         </div>
                     </div>
-                </div>
-            </li>
+                </li>
+         <?php endforeach; ?>   
         </ul>
     </section>
 </main>
@@ -106,24 +107,12 @@ $user_avatar = 'img/user.jpg';
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <li class="nav__item">
-                <a href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Крепления</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Одежда</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Разное</a>
-            </li>
+                <!-- Выводит меню футера -->
+            <?php foreach ($categories as $i => $category):  ?>
+
+                <li class="nav__item"><a href="../pages/all-lots.html"><?=$category['title']; ?></a></li>
+
+            <?php endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
