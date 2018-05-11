4340d051d4e54d8ea73aaf18dbf87adbce791da1
Zhenchick <evgesha_37rus@mail.ru>
2018-05-11 11:22:56 +0300

    Шаблонизация проекта Fixed bug

--- index.php
+++ index.php
@@ -7,14 +7,14 @@ require "data.php";
 //HTML код главной страницы
 $page_content = renderTemplate('templates/index.php', [
     'categories' => $categories,
-    'lot' => $lots_list
+    'lots_list' => $lots_list
 ]);
 
 //Окончательный HTML код 
 $layout_content = renderTemplate('templates/layout.php',[
     'categories' => $categories,
     'content' => $page_content,
-     'title' => 'Главная'
+    'title' => 'Главная'
     ]
 );
 
--- templates/index.php
+++ templates/index.php
@@ -20,7 +20,27 @@
         <ul class="lots__list">
             <!-- Выводит лоты аукциона -->
             <?php foreach ($lots_list as $lot):  ?>
-                 <?=renderTemplate('templates/lots.php', ['lot' => $lots_list]);?> 
+                 <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?=$lot['img'] ?>" width="350" height="260" alt="<?=$lot['name'] ?>">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$lot['category'] ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot['name'] ?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                              <span class="lot__amount">Стартовая цена</span>
+                              <span class="lot__cost">
+                                <!-- Форматирует начальную цену, добавляет знак рубля -->
+                                <?=formatPrice($lot['price']) ?>
+                              </span>
+                            </div>
+                        <div class="lot__timer timer">
+
+                        </div>
+                      </div>
+                    </div>
+                 </li> 
             <?php endforeach; ?>   
         </ul>
 </section>
